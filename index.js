0ed38a5d5c799362ca648cc208e43e21da1a0cb4
GuillaumeDSM <g.dsm@laposte.net>
2019-04-04 17:01:28 +0200

    Test1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+// Comment
 app.use(express.static('dist'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
